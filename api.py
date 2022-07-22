d49b5bec9a1bb399db6c2a089b62d2e70f425305
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-07-22 14:21:26 +1000

    Create api.py

---
+++ api.py
@@ -0,0 +1,11 @@
+import os
+import json
+import sys
+import re
+import time
+
+def start(**kwargs):
+  print("Finding CloudWake server...")
+  time.sleep(4)
+  print("Woken up CloudWake server.")
+  print("Setup successful.")
